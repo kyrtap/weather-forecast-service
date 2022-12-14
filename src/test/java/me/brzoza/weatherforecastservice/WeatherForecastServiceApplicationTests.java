8028c2805d6ec52b4d479687cf58d17333ed6370
Patryk Brzoza <kyrtap@users.noreply.github.com>
2022-12-14 11:53:18 +0100

    Initial commit

---
+++ src/test/java/me/brzoza/weatherforecastservice/WeatherForecastServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brzoza.weatherforecastservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
